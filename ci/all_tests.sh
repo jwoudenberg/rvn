c6fd206e4fe3512d66d686102182527d7298f9e1
Ian McLerran <imclerran@protonmail.com>
2024-07-08 15:43:10 -0500

    Add CI workflow and bundle workflow

---
+++ ci/all_tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+roc='./roc_nightly/roc'
+# roc=$(which roc) # for local use
+
+src_dir='./package'
+
+# test-stub.roc
+
+# check and test package
+$roc check $src_dir/main.roc
+$roc test $src_dir/main.roc --linker=legacy
